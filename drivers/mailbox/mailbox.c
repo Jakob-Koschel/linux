af2c26a7e92e56873d2556e1c28af197744afaa8
Jakob Koschel <jakobkoschel@gmail.com>
2022-02-27 17:19:51 +0100

    mailbox: use dedicated list iterator variable
    To move the list iterator variable into the list_for_each_entry_*()
    macro in the future it should be avoided to use the list iterator
    variable after the loop body.
    
    To *never* use the list iterator variable after the loop it was
    concluded to use a separate iterator variable [1].
    
    Link: https://lore.kernel.org/all/CAHk-=wgRr_D8CB-D9Kg-c=EHreAsk5SqXPwr9Y7k9sA6cWXJ6w@mail.gmail.com/
    Signed-off-by: Jakob Koschel <jakobkoschel@gmail.com>

--- drivers/mailbox/mailbox.c
+++ drivers/mailbox/mailbox.c
@@ -331,7 +331,7 @@ EXPORT_SYMBOL_GPL(mbox_flush);
 struct mbox_chan *mbox_request_channel(struct mbox_client *cl, int index)
 {
 	struct device *dev = cl->dev;
-	struct mbox_controller *mbox;
+	struct mbox_controller *mbox = NULL, *iter;
 	struct of_phandle_args spec;
 	struct mbox_chan *chan;
 	unsigned long flags;
@@ -352,11 +352,13 @@ struct mbox_chan *mbox_request_channel(struct mbox_client *cl, int index)
 	}
 
 	chan = ERR_PTR(-EPROBE_DEFER);
-	list_for_each_entry(mbox, &mbox_cons, node)
-		if (mbox->dev->of_node == spec.np) {
-			chan = mbox->of_xlate(mbox, &spec);
-			if (!IS_ERR(chan))
+	list_for_each_entry(iter, &mbox_cons, node)
+		if (iter->dev->of_node == spec.np) {
+			chan = iter->of_xlate(iter, &spec);
+			if (!IS_ERR(chan)) {
+				mbox = iter;
 				break;
+			}
 		}
 
 	of_node_put(spec.np);
